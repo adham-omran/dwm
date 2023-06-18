2fbf4bd355f59714c1d20f911b12feebed854337
adham-omran <git@adham-omran.com>
2023-06-18 23:22:10 +0300

    Add the systray patch

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono:size=14" };
@@ -57,18 +62,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-/* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
 static const char *dmenucmd[] = {"rofi", "-show", "drun"};
-static const char *termcmd[]  = { "xterm", NULL };
+static const char *termcmd[]  = { "st", NULL};
 static const char *emacscmd[] = {"emacsclient", "-s", "main", "-c", NULL};
 static const char *file_manager_cmd[] = {"nemo", NULL};
 static const char *screenshot_cmd[] = {"scrot", "-fs", "-F", "/home/adham/pics/screenshots/$(date +%Y-%m-%d-%H:%M:%S).png",
-				       "-e", "'xclip", "-selection", "clipboard", "-t", "image/png", "-i", "$f'"};
+				       "-e", "'xclip", "-selection", "clipboard", "-t", "image/png", "-i", "$f'", NULL};
 
-static const char *volume_up_cmd[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%+"};
-static const char *volume_down_cmd[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%-"};
-static const char *volume_mute_cmd[] = {"wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle"};
-static const char *volume_mute_mic_cmd[] = {"wpctl", "set-mute", "@DEFAULT_SOURCE@", "toggle"};
+static const char *volume_up_cmd[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%+", NULL};
+static const char *volume_down_cmd[] = {"wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "2%-", NULL};
+static const char *volume_mute_cmd[] = {"wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL};
+static const char *volume_mute_mic_cmd[] = {"wpctl", "set-mute", "@DEFAULT_SOURCE@", "toggle", NULL};
 
 static const Key keys[] = {
   /* modifier                     key        function        argument */
@@ -118,8 +122,8 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
